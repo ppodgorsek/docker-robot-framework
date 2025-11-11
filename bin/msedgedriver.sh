6347c278a3a86269d0393747f50db171c04b110c
Paul Podgorsek <ppodgorsek@lefoodlink.com>
2025-11-11 19:51:38 +0000

    Added verbose logs for the Edge driver

--- bin/msedgedriver.sh
+++ bin/msedgedriver.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-exec /opt/robotframework/drivers/msedgedriver-original --verbose --log-path=/var/log/msedgedriver "$@"
-
-cat /var/log/msedgedriver
+exec /opt/robotframework/drivers/msedgedriver-original --verbose --log-path=/var/log/msedgedriver "$@" || cat /var/log/msedgedriver
